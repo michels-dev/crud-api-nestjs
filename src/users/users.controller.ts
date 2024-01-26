ee6522cc283d270eed402b0c4186c4b4d490c777
michelsdev <sinambelamichel05@gmail.com>
2024-01-26 10:24:02 +0700

    add users.controller.ts

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,48 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, NotFoundException, Param, Post, Put } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { User } from './user.entity';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private readonly usersService: UsersService){}
+
+    // get all users
+    @Get()
+    async findAll(): Promise<User[]>{
+        return this.usersService.findAll();
+    }
+
+    // get user by id
+    @Get(':id')
+    async findOne(@Param('id') id: number): Promise<User> {
+        const user = await this.usersService.findOne(id);
+        if(!user) {
+            throw new NotFoundException('User does not exist');
+        } else {
+            return user;
+        }
+    }
+
+    // create user
+    @Post()
+    async create(@Body() user: User): Promise<User> {
+        return this.usersService.create(user);
+    }
+
+    // update user
+    @Put(':id')
+    async update(@Param('id') id: number, @Body() user: User): Promise<any> {
+        return this.usersService.update(id, user);
+    }
+
+    // delete user
+    @Delete(':id')
+    async delete(@Param('id') id: number): Promise<any> {
+        // handle error if user does not exist
+        const user = await this.usersService.findOne(id);
+        if(!user){
+            throw new NotFoundException('User not does not exist!');
+        }
+        return this.usersService.delete(id);
+    }
+}
