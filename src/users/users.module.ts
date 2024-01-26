36853a56707c70e64300366fbc23f4a92111d51a
michelsdev <sinambelamichel05@gmail.com>
2024-01-26 11:15:07 +0700

    add users.module.ts

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user.entity';
 
-@Module({
+@Module({ // menggunakan dekorator untuk memberi tahu nestjs bahwa ini adalah sebuah model
+  // mengimpor TypeOrmModule dan entitas pengguna (UserController dan UserService sudah diimpor)
+  imports: [TypeOrmModule.forFeature([User])], // menambahkan [User] ke larik impor untuk memberi tahu nestjs bahwa kita ingin menggunakan entitas User
   controllers: [UsersController],
   providers: [UsersService]
 })
