efb4f567b67af47769f80b3f29004c20d4b2ce65
michelsdev <sinambelamichel05@gmail.com>
2024-01-26 08:40:40 +0700

    add user.entity.ts

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,11 +1,14 @@
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
+// menggunakan dekorator untuk memberi tahu typeorm bahwa ini adalah entitas
 @Entity()
 export class User {
+    // menggunakan dekorator untuk memberi tahu typeorm bahwa ini adalah kunci utama dari table
     @PrimaryGeneratedColumn()
     id: number;
 
-    @Column()
+    // menggunakan dekorator untuk memberi tahu typeorm bahwa ini adalah kolom dari table
+    @Column({length:500})
     name: string;
 
     @Column()
