77e4f319b0fa481fc8ccedbb300194151ef9c7c6
michelsdev <sinambelamichel05@gmail.com>
2024-01-27 12:08:31 +0700

    app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule],
+  imports: [ // mengimport ConfigModule, UsersModule, TypeOrmModule
+    ConfigModule.forRoot(), // mengimport ConfigModule, UsersModule, TypeOrmModule dalam array
+    UsersModule,
+    TypeOrmModule.forRoot({ // menggunakan forRoot untuk memberitahu nestjs menggunakan koneksi default, mendefinisikan variabel untuk menyambungkan ke basis data
+      type: process.env.DB_TYPE as any, // mengatur dalam berkas docker-compose.yml
+      host: process.env.PG_HOST,
+      port: parseInt(process.env.PG_PORT),
+      username: process.env.PG_USER,
+      password: process.env.PG_PASSWORD,
+      database: process.env.PG_DB,
+      entities:[__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true, // true untuk membuat skema basis data secara otomatis diperbarui ketika aplikasi dimulai
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
