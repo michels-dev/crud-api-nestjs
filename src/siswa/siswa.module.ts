8d43ae75b0b595e4710a7c09a8cb2e95875f1535
michelsdev <sinambelamichel05@gmail.com>
2024-01-29 13:41:46 +0700

    siswa.modulests

--- src/siswa/siswa.module.ts
+++ src/siswa/siswa.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { SiswaController } from './siswa.controller';
 import { SiswaService } from './siswa.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSiswa } from './datasiswa.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([DataSiswa])],
   controllers: [SiswaController],
   providers: [SiswaService]
 })
