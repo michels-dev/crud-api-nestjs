07c26a7e5b08f1c709591b6260f222e6a184b3af
michelsdev <sinambelamichel05@gmail.com>
2024-01-29 09:43:43 +0700

    packages siswa dan sintaks user.entity.ts

---
+++ src/siswa/datasiswa.entity.ts
@@ -0,0 +1,70 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class DataSiswa {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ type: "timestamp" })
+    date_time: Date;
+
+    @Column()
+    modified: Date;
+
+    @Column()
+    ip: string;
+
+    @Column()
+    creator: number;
+
+    @Column()
+    modifier: number;
+
+    @Column()
+    nospj: string;
+
+    @Column({ type: "text" })
+    spj: string;
+
+    @Column()
+    kodesek: string;
+
+    @Column()
+    kelas: string;
+
+    @Column()
+    noinduk: string;
+
+    @Column()
+    no_urut_kelas: string;
+
+    @Column()
+    no_urut_buku_induk: string;
+
+    @Column()
+    nik_guru_term_1: string;
+
+    @Column()
+    nik_guru_term_2: string;
+
+    @Column()
+    nik_guru_term_3: string;
+
+    @Column()
+    nik_guru_term_4: string;
+
+    @Column()
+    namasiswa: string;
+
+    @Column()
+    aktif: string;
+
+    @Column({ type: "text" })
+    foto: string;
+
+    @Column()
+    keterangan: string;
+
+    @Column()
+    status_aktif_sekolah: number;
+}
